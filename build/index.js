a81e18bf21161235122aa20c4cbd24657ed54177
Pablo Acosta <pablo.acosta@nftstudios.io>
2025-01-03 09:55:13 -0300

    feat: :sparkles: Add build

--- build/index.js
+++ build/index.js
@@ -1,13 +1,36 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv").config();
-const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
 const axios_1 = __importDefault(require("axios"));
-const cheerio_1 = __importDefault(require("cheerio"));
+const cheerio = __importStar(require("cheerio"));
+const cors_1 = __importDefault(require("cors"));
+const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.get("/", (req, res) => {
@@ -46,16 +69,16 @@ app.get("/lyrics", (req, res) => {
         .request(options)
         .then((response) => {
         const html = response.data;
-        const $ = cheerio_1.default.load(html);
+        const $ = cheerio.load(html);
         let lyrics = [];
-        $("div[class*='Lyrics__Container']", html).each(function () {
+        $("div[class*='Lyrics-sc-1bcc94c6-1 bzTABU']", html).each(function () {
             let scrappedLyrics = $(this);
             scrappedLyrics.find("a").each(function () {
                 $(this).replaceWith($(this).find("span").html());
             });
             lyrics.push(scrappedLyrics.html());
         });
-        res.send(lyrics);
+        res.send(lyrics[0]);
     })
         .catch((err) => console.log(err));
 });
